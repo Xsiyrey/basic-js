48d89757a8ca142a5de7fe694f19e480e2cb8174
Вячеслав Курганов <sgorelchik@outlook.com>
2021-02-25 16:00:26 +0300

    feat: calc date sample

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,11 @@
-const CustomError = require("../extensions/custom-error");
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== "string" || !Number.isInteger(sampleActivity))
+    return false;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  return Math.round(
+    Math.log(MODERN_ACTIVITY / +sampleActivity) / (0.693 / HALF_LIFE_PERIOD)
+  );
 };
