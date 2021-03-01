dd524790a4061978774d8f12d4dd454a28b64f9f
Вячеслав Курганов <sgorelchik@outlook.com>
2021-03-01 08:12:18 +0300

    feat: calc dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  return members
+    .reduce(
+      (acc, cur) =>
+        cur && typeof cur === "string"
+          ? acc + cur.trim()[0].toUpperCase()
+          : acc,
+      ""
+    )
+    .split("")
+    .sort()
+    .join("");
+  throw new CustomError("Not implemented");
   // remove line with error and write your code here
 };
